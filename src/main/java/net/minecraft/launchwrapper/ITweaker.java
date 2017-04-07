04d7a3910e4cc9dc0961601f1603a942c48123da
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-08 01:51:23 +0300

    Retain compatibility with original Tweaker interface

--- src/main/java/net/minecraft/launchwrapper/ITweaker.java
+++ src/main/java/net/minecraft/launchwrapper/ITweaker.java
@@ -1,10 +1,19 @@
 package net.minecraft.launchwrapper;
 
+import java.io.File;
 import java.util.List;
 
 public interface ITweaker {
-    void acceptOptions(List<String> args);
     void injectIntoClassLoader(LaunchClassLoader classLoader);
     String getLaunchTarget();
     String[] getLaunchArguments();
+
+    default void acceptOptions(List<String> args) {
+        acceptOptions(args, null, null, null);
+    }
+
+    @Deprecated
+    default void acceptOptions(List<String> args, File gameDir, final File assetsDir, String profile) {
+        throw new IllegalStateException("Please implement this method.");
+    }
 }
